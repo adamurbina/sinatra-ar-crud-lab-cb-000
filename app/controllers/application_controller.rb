04cf243e23619c9a151b58bb7b9734e47c651612
Adam Urbina <adamurbina@gmail.com>
2018-08-11 13:29:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/posts" do
-      @post = Post.create(params)
+      post = Post.create(params)
       @posts = Post.all
 
       erb :index
